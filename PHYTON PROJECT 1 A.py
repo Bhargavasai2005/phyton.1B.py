7012c326d070f7d6dde2f153376bd4a8334cdf41
Bhargavasai2005 <94349388+Bhargavasai2005@users.noreply.github.com>
2022-06-30 20:26:20 +0530

    Add files via upload

---
+++ PHYTON PROJECT 1 A.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 20:03:19 2022
+
+@author: ADMIN
+"""
+
+filename=input("input the filename: ")
+f_extns = filename.split(".")
+print("the elemnet of the file is:"+ (f_extns[-1]))
